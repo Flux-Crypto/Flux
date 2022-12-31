48e020800ec710770fdac8e722ca80e2ab86c82d
T <totran0331@gmail.com>
2022-12-31 18:11:21 -0500

    Set up ESLint custom config

---
+++ apps/backend/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ...require("@aurora/eslint-config-custom/.eslintrc")
+    // parserOptions: {
+    //     root: true,
+    //     tsconfigRootDir: __dirname,
+    //     project: ["./tsconfig.json"]
+    // }
+};
