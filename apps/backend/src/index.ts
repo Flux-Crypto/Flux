b850c3bec4029b6a52c0fcc2c66e753b1cc1eb9d
T <totran0331@gmail.com>
2022-12-31 19:00:28 -0500

    Fetch blockchain transactions

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,11 @@
-import fastify, { FastifyInstance } from "fastify";
+import prisma from "@backend/plugins/prisma";
+import transactions from "@backend/routes/transactions";
+import user from "@backend/routes/user";
+import users from "@backend/routes/users";
 import swagger from "@fastify/swagger";
 // @ts-ignore:next-line
 import swaggerUI from "@fastify/swagger-ui";
+import fastify, { FastifyInstance } from "fastify";
 
 import SwaggerOptions from "../docs/options";
 import { prismaPlugin } from "./plugins";
@@ -20,15 +24,14 @@ const runServer = async () => {
     server.register(
         (server: FastifyInstance, _opts: any, done: () => void) => {
             server.register(users, { prefix: "/users" });
-
+            server.register(transactions, { prefix: "/transactions" });
+            
             done();
         },
         { prefix: "/api/v1" }
     );
 
-    server.get("/ping", async (_request, _reply) => {
-        return "pong\n";
-    });
+    server.get("/ping", async (_request, _reply) => "pong\n");
 
     await server.ready();
     server.listen({ port: 8000 }, (err, address) => {
--- apps/backend/src/plugins/prisma.ts
+++ apps/backend/src/plugins/prisma.ts
@@ -1,25 +1,25 @@
-import fp from "fastify-plugin"
-import { FastifyPluginAsync } from "fastify"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import { FastifyPluginAsync } from "fastify";
+import fp from "fastify-plugin";
 
 // Use TypeScript module augmentation to declare the type of server.prisma to be PrismaClient
 declare module "fastify" {
-   interface FastifyInstance {
-      prisma: PrismaClient
-   }
+    interface FastifyInstance {
+        prisma: PrismaClient;
+    }
 }
 
-const prismaPlugin: FastifyPluginAsync = fp(async (server, options) => {
-   const prisma = new PrismaClient()
+const prismaPlugin: FastifyPluginAsync = fp(async (server) => {
+    const prisma = new PrismaClient();
 
-   await prisma.$connect()
+    await prisma.$connect();
 
-   // Make Prisma Client available through the fastify server instance: server.prisma
-   server.decorate("prisma", prisma)
+    // Make Prisma Client available through the fastify server instance: server.prisma
+    server.decorate("prisma", prisma);
 
-   server.addHook("onClose", async (server) => {
-      await server.prisma.$disconnect()
-   })
-})
+    server.addHook("onClose", async (serv) => {
+        await serv.prisma.$disconnect();
+    });
+});
 
-export default prismaPlugin
+export default prismaPlugin;
