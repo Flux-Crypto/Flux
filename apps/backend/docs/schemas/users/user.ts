2abac35c7cd5d6bced2df6c869319625be737b62
dtran <duketran2001@gmail.com>
2023-01-02 02:13:38 -0500

    abstracted models from swagger schemas

---
+++ apps/backend/docs/schemas/users/user.ts
@@ -0,0 +1,35 @@
+import { UserSchema } from "../apiSchema";
+
+export default {
+    get: {
+        schema: {
+            description: "Gets user data based on supplied user id.",
+            tags: ["users"],
+            summary: "Gets user data.",
+            params: {
+                type: "object",
+                properties: {
+                    userId: {
+                        type: "string",
+                        description: "user id"
+                    }
+                }
+            },
+            response: {
+                "200": {
+                    description: "OK",
+                    type: "object",
+                    properties: UserSchema
+                },
+                "400": {
+                    description: "Bad request. Missing user id parameter.",
+                    type: "null"
+                },
+                "5XX": {
+                    description: "Unexpected error.",
+                    type: "null"
+                }
+            }
+        }
+    }
+};
