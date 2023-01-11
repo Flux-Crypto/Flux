59129853c9a3479aee79db398813a983e6b5fb6f
dtran <duketran2001@gmail.com>
2023-01-11 02:13:52 -0500

    #86770t34j refactored wallets manage page

--- apps/frontend/src/pages/dashboard/wallets/manage.tsx
+++ apps/frontend/src/pages/dashboard/wallets/manage.tsx
@@ -4,19 +4,61 @@ import {
     Stack,
     Text,
     ThemeIcon,
+    ThemeIconProps,
     Title,
     Tooltip
 } from "@mantine/core";
-import { IconEyeglass, IconPencil } from "@tabler/icons";
+import { IconEyeglass, IconPencil, TablerIcon } from "@tabler/icons";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 import { UserSession } from "@lib/types/auth";
-import { UserWallets } from "@lib/types/db";
+import { UserWallet, UserWallets } from "@lib/types/db";
 import callAPI from "@src/lib/callAPI";
 
 import WalletCard from "@components/WalletCard";
 import DashboardLayout from "@layouts/DashboardLayout";
+
+interface WalletGridProps {
+    accessTitle: string;
+    tooltipLabel: string;
+    TitleIcon: TablerIcon;
+    iconProps: Omit<ThemeIconProps, "children">;
+    wallets: UserWallet[];
+}
+
+const WalletGrid = ({
+    accessTitle,
+    tooltipLabel,
+    TitleIcon,
+    iconProps,
+    wallets
+}: WalletGridProps) => (
+    <Stack>
+        <Group>
+            <ThemeIcon radius="sm" {...iconProps} p={2}>
+                <TitleIcon />
+            </ThemeIcon>
+
+            <Title order={3}>
+                <Tooltip label={tooltipLabel}>
+                    <Text underline span>
+                        {accessTitle}
+                    </Text>
+                </Tooltip>{" "}
+                Wallets
+            </Title>
+        </Group>
+
+        <Grid columns={2} gutter="xl">
+            {wallets.map((wallet) => (
+                <Grid.Col span={1}>
+                    <WalletCard key={wallet.address} {...{ ...wallet }} />
+                </Grid.Col>
+            ))}
+        </Grid>
+    </Stack>
+);
 
 const Manage = () => {
     const { data: session } = useSession();
@@ -50,74 +92,33 @@ const Manage = () => {
     }, [isFetching, session, wallets]);
 
     const { rdWallets, rdwrWallets } = wallets;
-    console.log(rdwrWallets);
 
     return (
         <DashboardLayout pageTitle="Manage Wallets">
             <Stack spacing={56}>
                 {rdWallets.length ? (
-                    <Stack>
-                        <Group>
-                            <ThemeIcon
-                                variant="light"
-                                radius="sm"
-                                color="indigo"
-                                p={2}
-                            >
-                                <IconEyeglass />
-                            </ThemeIcon>
-
-                            <Title order={3}>
-                                <Tooltip label="Wallets that you can view and track.">
-                                    <Text underline span>
-                                        Read-only
-                                    </Text>
-                                </Tooltip>{" "}
-                                Wallets
-                            </Title>
-                        </Group>
-
-                        <Grid columns={2} gutter="xl">
-                            {rdWallets.map((wallet) => (
-                                <Grid.Col span={1}>
-                                    <WalletCard
-                                        key={wallet.address}
-                                        {...{ ...wallet }}
-                                    />
-                                </Grid.Col>
-                            ))}
-                        </Grid>
-                    </Stack>
+                    <WalletGrid
+                        accessTitle="Read-only"
+                        tooltipLabel="Wallets that you can view and track."
+                        TitleIcon={IconEyeglass}
+                        iconProps={{
+                            variant: "light",
+                            color: "indigo"
+                        }}
+                        wallets={rdWallets}
+                    />
                 ) : null}
 
                 {rdwrWallets.length ? (
-                    <Stack>
-                        <Group>
-                            <ThemeIcon radius="sm" color="pink" p={2}>
-                                <IconPencil />
-                            </ThemeIcon>
-
-                            <Title order={3}>
-                                <Tooltip label="Wallets that you can access and use to transact.">
-                                    <Text underline span>
-                                        Read-write
-                                    </Text>
-                                </Tooltip>{" "}
-                                Wallets
-                            </Title>
-                        </Group>
-
-                        <Grid columns={2} gutter="xl">
-                            {rdwrWallets.map((wallet) => (
-                                <Grid.Col span={1}>
-                                    <WalletCard
-                                        key={wallet.address}
-                                        {...{ ...wallet }}
-                                    />
-                                </Grid.Col>
-                            ))}
-                        </Grid>
-                    </Stack>
+                    <WalletGrid
+                        accessTitle="Read-write"
+                        tooltipLabel="Wallets that you can access and use to transact."
+                        TitleIcon={IconPencil}
+                        iconProps={{
+                            color: "pink"
+                        }}
+                        wallets={rdwrWallets}
+                    />
                 ) : null}
 
                 {!rdWallets.length && !rdwrWallets.length && (
